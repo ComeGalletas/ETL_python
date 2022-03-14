8afa6b2fe236e79ae051a6518f19e88281a10ed4
Juan Saenz <juanpa_507@hotmail.com>
2022-03-14 11:21:03 -0500

    First commit

---
+++ ETL_corresponsal.py
@@ -0,0 +1,15 @@
+from __future__ import print_function
+from unicodedata import normalize
+
+import pandas as pd
+import numpy as np
+
+
+
+
+def main():
+    print("main")
+
+
+if __name__ == '__main__':
+    main()
